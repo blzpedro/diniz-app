8a9d63e9ca397719029a2cb079e9753f8c493466
Pedro Melo <pedro.melo@performait.com>
2021-08-18 00:07:04 -0300

    changes + add firebase and deploy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'diniz-app';
+
+  ngOnInit() {
+    this.showIosInstallBanner()
+  }
+
+  async showIosInstallBanner() {
+    // Detects if device is on iOS
+    const isIos = () => {
+      const userAgent = window.navigator.userAgent.toLowerCase();
+      console.log(userAgent)
+      return /iphone|ipad|ipod/.test(userAgent);
+    };
+
+    if (isIos()) {
+      alert('ta no iOS, update')
+    } else {
+      alert('não ta no iOS, update')
+    }
+  }
 }
